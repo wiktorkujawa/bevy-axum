304b5d3f0134edd9b11db9952d245a694c750d8e
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-07-31 15:12:58 +0200

    update level instantiation data

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,30 @@
+use axum::response::Html;
+use axum::routing::get;
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
 use tower_http::cors::CorsLayer;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let layer = CorsLayer::permissive();
-    let router = Router::new().nest_service(
+    let router = Router::new()
+    .nest_service(
         "/",
         ServeDir::new("dist").not_found_service(ServeFile::new("dist/index.html")),
-    ).layer(layer);
+    )
+    // .nest_service(
+    //     "/assets",
+    //     ServeDir::new("dist/assets"),
+    // )
+    // .route("/app", get(index))
+    .layer(layer);
+
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
+
+// // Include utf-8 file at **compile** time.
+// async fn index() -> Html<&'static str> {
+//     Html(std::include_str!("../../dist/index.html"))
+// }
