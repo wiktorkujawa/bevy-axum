f0987102f65e9c45b2da84c168baf1a11403b532
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-07-31 16:10:41 +0200

    fixed build and level.ron

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,3 @@
-use axum::response::Html;
-use axum::routing::get;
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
 use tower_http::cors::CorsLayer;
@@ -12,19 +10,9 @@ async fn main() {
         "/",
         ServeDir::new("dist").not_found_service(ServeFile::new("dist/index.html")),
     )
-    // .nest_service(
-    //     "/assets",
-    //     ServeDir::new("dist/assets"),
-    // )
-    // .route("/app", get(index))
     .layer(layer);
 
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
-
-// // Include utf-8 file at **compile** time.
-// async fn index() -> Html<&'static str> {
-//     Html(std::include_str!("../../dist/index.html"))
-// }
